f64e4ba19a928d151ce0cd730570c7a637098701
anhtumai <tu.mai@outlook.com>
2021-07-06 22:02:10 +0300

    countries app, refractor CountriesDisplay component

--- part2/countries/src/App.tsx
+++ part2/countries/src/App.tsx
@@ -1,34 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-
-import Filter from './components/Filter'
-import CountriesDisplay, { ICountryData } from './components/CountriesDisplay'
+import Filter from "./components/Filter";
+import CountriesDisplay, { ICountryData } from "./components/CountriesDisplay";
 
 const App = () => {
-  const [filter, setFilter] = useState('')
-  const [countries, setCountries] = useState<ICountryData[] | []>([])
-  const [country, setCountry] = useState<ICountryData>()
+  const [filter, setFilter] = useState("");
+  const [countries, setCountries] = useState<ICountryData[]>([]);
+  const [country, setCountry] = useState<ICountryData | undefined>();
 
   useEffect(() => {
-    axios
-      .get('https://restcountries.eu/rest/v2/all')
-      .then(response => {
-        setCountries(response.data)
-      })
-  }, [])
+    axios.get("https://restcountries.eu/rest/v2/all").then((response) => {
+      setCountries(response.data);
+    });
+  }, []);
 
   const handleFilter = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setCountry(undefined)
-    setFilter(e.target.value)
-  }
+    setCountry(undefined);
+    setFilter(e.target.value);
+  };
+
+  const displayedCountries = countries.filter((country) =>
+    country.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
   return (
     <div>
       <Filter filter={filter} handleFilter={handleFilter} />
-      <CountriesDisplay filter={filter} countries={countries} country={country} setCountry={setCountry} />
+      <CountriesDisplay
+        countries={displayedCountries}
+        country={country}
+        setCountry={setCountry}
+      />
     </div>
-  )
-}
+  );
+};
 
 export default App;
