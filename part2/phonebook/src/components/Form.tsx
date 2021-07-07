df1ef1819847b8dd11c6388c798dbfdbff9de312
anhtumai <tu.mai@outlook.com>
2021-07-08 02:03:06 +0300

    Add validation for submitted name and phone number

--- part2/phonebook/src/components/Form.tsx
+++ part2/phonebook/src/components/Form.tsx
@@ -11,6 +11,34 @@ interface IPersonFormProps {
   handleSubmit(submittedInfo: IFormInfo): void;
 }
 
+/**
+ * Phone number is valid if it only contains digit and '-'.
+ * '-' must also be surrounded by digits
+ *
+ * @param {string} phoneNumber
+ * @returns {boolean}
+ */
+function validatePhoneNumber(phoneNumber: string): boolean {
+  if (phoneNumber.length < 8 || phoneNumber.length >= 16) return false;
+  for (let i = 0; i < phoneNumber.length; i++) {
+    const c = phoneNumber[i];
+    if ((c < "0" || c > "9") && c !== "-") return false;
+    if (i > 0 && c === "-" && phoneNumber[i - 1] === "-") return false;
+  }
+  if (phoneNumber[0] === "-" || phoneNumber.slice(-1) === "-") return false;
+  return true;
+}
+
+/**
+ * Check submitted name is valid
+ *
+ * @param {string} name
+ * @returns {boolean}
+ */
+function validateName(name: string): boolean {
+  return name.length > 0;
+}
+
 const PersonForm = ({ handleSubmit }: IPersonFormProps) => {
   const [name, setName] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
@@ -22,11 +50,14 @@ const PersonForm = ({ handleSubmit }: IPersonFormProps) => {
 
   const onSubmit = (event: React.SyntheticEvent) => {
     event.preventDefault();
-    handleSubmit(submittedInfo);
+    if (validatePhoneNumber(phoneNumber) && validateName(name)) {
+      const submittedInfo = { name, number: phoneNumber };
+      handleSubmit(submittedInfo);
+    } else {
+      alert(`Submitted phone number or name is not valid`);
+    }
     resetInputFields();
   };
-
-  const submittedInfo = { name, number: phoneNumber };
 
   return (
     <>
